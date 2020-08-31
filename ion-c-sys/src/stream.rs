ade8467e4dbf27768fa6d2f7947fc8d7690dda52
Almann Goo <almann@amazon.com>
2020-08-31 11:02:35 -0700

    Removes some dead code.

--- ion-c-sys/src/stream.rs
+++ ion-c-sys/src/stream.rs
@@ -7,30 +7,6 @@ use std::io::Write;
 
 use crate::*;
 
-/// Represents an input stream for Ion C.  
-#[repr(C)]
-struct IonCReadStream<T: BufRead> {
-    input: T,
-}
-
-impl<T: BufRead> IonCReadStream<T> {
-    pub fn new(input: T) -> Self {
-        Self { input }
-    }
-}
-
-/// Represents an output stream for Ion C.
-#[repr(C)]
-struct IonCWriteStream<T: Write> {
-    output: T,
-}
-
-impl<T: Write> IonCWriteStream<T> {
-    pub fn new(output: T) -> Self {
-        Self { output }
-    }
-}
-
 /// Bridges the raw C interface for the `_ion_user_stream` handler to something a bit more Rust
 /// friendly and can use a closure environment to handle the Rust context generically.
 #[repr(C)]
