ef698f878ce43bdb97250ca9f1a3d2c7993a2d12
Almann Goo <almann@amazon.com>
2020-09-02 12:22:14 -0700

    More incremental implementation.

--- ion-c-sys/src/stream.rs
+++ ion-c-sys/src/stream.rs
@@ -10,24 +10,31 @@ use crate::*;
 /// Bridges the raw C interface for the `_ion_user_stream` handler to something a bit more Rust
 /// friendly and can use a closure environment to handle the Rust context generically.
 #[repr(C)]
-pub struct IonCStreamHandler<F: FnMut(&mut _ion_user_stream) -> IonCResult<()>> {
+pub struct IonCStreamHolder<'a, T, F: Fn(&mut T, &mut _ion_user_stream) -> IonCResult<()>> {
+    source: &'a mut T,
     handler: F,
 }
 
-impl<F: FnMut(&mut _ion_user_stream) -> IonCResult<()>> IonCStreamHandler<F> {
+impl<'a, T, F> IonCStreamHolder<'a, T, F>
+where
+    F: Fn(&mut T, &mut _ion_user_stream) -> IonCResult<()>,
+{
     /// Wraps the given closure into an FFI safe structure.
-    pub fn new(handler: F) -> Self {
-        Self { handler }
+    pub fn new(source: &'a mut T, handler: F) -> Self {
+        Self { source, handler }
     }
 }
 
-unsafe extern "C" fn ionc_stream_handler<F>(stream: *mut _ion_user_stream) -> iERR
+/// Ion C callback to bridge to our handler.
+unsafe extern "C" fn ionc_stream_handler<'a, T, F>(stream: *mut _ion_user_stream) -> iERR
 where
-    F: FnMut(&mut _ion_user_stream) -> IonCResult<()>,
+    T: 'a,
+    F: Fn(&mut T, &mut _ion_user_stream) -> IonCResult<()> + 'a,
 {
     let user_stream = stream.as_mut().unwrap();
-    let handler_obj_ptr = user_stream.handler_state as *mut IonCStreamHandler<F>;
-    let result = ((*handler_obj_ptr).handler)(user_stream);
+    let holder_ptr = user_stream.handler_state as *mut IonCStreamHolder<'a, T, F>;
+    let holder = holder_ptr.as_mut().unwrap();
+    let result = (holder.handler)(holder.source, user_stream);
     match result {
         Ok(_) => ion_error_code_IERR_OK,
         Err(e) => e.code,
