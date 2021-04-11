757f0b5d4d29758e7c14e8a08ed44a58bfc9a0ba
Almann Goo <almann@amazon.com>
2021-04-11 12:41:52 -0700

    Adds conversion from `ion_c_sys::ION_TYPE` to `IonType`.
    TODO testing.
    
    Resolves #178.

--- src/types/type.rs
+++ src/types/type.rs
@@ -1,3 +1,7 @@
+use crate::result::{illegal_operation, IonError};
+use ion_c_sys::ION_TYPE;
+use std::convert::TryFrom;
+
 /// Represents the Ion data type of a given value. To learn more about each data type,
 /// read [the Ion Data Model](http://amzn.github.io/ion-docs/docs/spec.html#the-ion-data-model)
 /// section of the spec.
@@ -24,6 +28,30 @@ impl IonType {
         match self {
             List | SExpression | Struct => true,
             _ => false,
+        }
+    }
+}
+
+impl TryFrom<ION_TYPE> for IonType {
+    type Error = IonError;
+
+    fn try_from(value: ION_TYPE) -> Result<Self, Self::Error> {
+        use IonType::*;
+        match value {
+            ion_c_sys::ION_TYPE_NULL => Ok(Null),
+            ion_c_sys::ION_TYPE_BOOL => Ok(Boolean),
+            ion_c_sys::ION_TYPE_INT => Ok(Integer),
+            ion_c_sys::ION_TYPE_FLOAT => Ok(Float),
+            ion_c_sys::ION_TYPE_DECIMAL => Ok(Decimal),
+            ion_c_sys::ION_TYPE_TIMESTAMP => Ok(Timestamp),
+            ion_c_sys::ION_TYPE_SYMBOL => Ok(Symbol),
+            ion_c_sys::ION_TYPE_STRING => Ok(String),
+            ion_c_sys::ION_TYPE_CLOB => Ok(Clob),
+            ion_c_sys::ION_TYPE_BLOB => Ok(Blob),
+            ion_c_sys::ION_TYPE_LIST => Ok(List),
+            ion_c_sys::ION_TYPE_SEXP => Ok(SExpression),
+            ion_c_sys::ION_TYPE_STRUCT => Ok(Struct),
+            _ => illegal_operation(format!("Could not convert Ion C ION_TYPE: ${:?}", value)),
         }
     }
 }
