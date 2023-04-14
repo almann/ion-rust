54940ab2e9de43bb3b4758af20d3959bf5cfe074
Almann Goo <almann.goo@gmail.com>
2023-04-14 16:17:03 -0700

    Adds Display implementation for most types

--- src/macros/types.rs
+++ src/macros/types.rs
@@ -5,9 +5,11 @@
 //! This goes above the runtime type system of Ion within the macro system.
 
 use crate::IonType;
+use std::fmt::{Display, Formatter};
 
 /// Macro types that are encoded without and fixed width.
 /// These types are structural constrained types over their general Ion equivalent.
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
 pub enum FixedType {
     UInt8,
     UInt16,
@@ -22,7 +24,30 @@ pub enum FixedType {
     Float64,
 }
 
+impl Display for FixedType {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                FixedType::UInt8 => "uint8",
+                FixedType::UInt16 => "uint16",
+                FixedType::UInt32 => "uint32",
+                FixedType::UInt64 => "uint64",
+                FixedType::Int8 => "int8",
+                FixedType::Int16 => "int16",
+                FixedType::Int32 => "int32",
+                FixedType::Int64 => "int64",
+                FixedType::Float16 => "float16",
+                FixedType::Float32 => "float32",
+                FixedType::Float64 => "float64",
+            }
+        )
+    }
+}
+
 /// Macro types that are _abstract_ over the type system.
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
 pub enum UnionType {
     /// The top-type, any and all Ion values are of this type.
     Any,
@@ -38,12 +63,83 @@ pub enum UnionType {
     Sequence,
 }
 
+impl Display for UnionType {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                UnionType::Any => "any",
+                UnionType::Number => "number",
+                UnionType::Exact => "exact",
+                UnionType::Text => "text",
+                UnionType::Lob => "lob",
+                UnionType::Sequence => "sequence",
+            }
+        )
+    }
+}
+
+/// Cardinality of arguments for a macro parameter.
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+pub enum Cardinality {
+    ExactlyOne,
+    ZeroOrOne,
+    ZeroOrMore,
+    OneOrMore,
+    Rest,
+}
+
+impl Display for Cardinality {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Cardinality::ExactlyOne => "!",
+                Cardinality::ZeroOrOne => "?",
+                Cardinality::ZeroOrMore => "*",
+                Cardinality::OneOrMore => "+",
+                Cardinality::Rest => "...",
+            }
+        )
+    }
+}
+
+/// Pair of type and cardinality
+pub struct ParamType(pub Box<StaticType>, pub Cardinality);
+
+impl Display for ParamType {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}{}", *self.0, self.1)
+    }
+}
+
+/// Return and parameters of a macro.
+pub struct SigType {
+    result: Box<StaticType>,
+    parameters: Vec<ParamType>,
+}
+
+impl Display for SigType {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
+
 /// Static types for the Ion macro system.
-pub enum Type {
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub enum StaticType {
     Union(UnionType),
     Tagged(IonType),
     Untagged(IonType),
     Fixed(FixedType),
-    // TODO this is parameterized
+    // TODO make this is parameterized
     Shape,
+}
+
+impl Display for StaticType {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
 }
