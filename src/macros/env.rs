fd13d206aefa52e5b12d88ce45c1f8d9fcf25fcc
Almann Goo <almann.goo@gmail.com>
2023-05-07 13:10:59 -0700

    Adds stub for Module

--- src/macros/env.rs
+++ src/macros/env.rs
@@ -18,19 +18,20 @@
 //!
 //! [1]: https://en.wikipedia.org/wiki/Persistent_data_structure
 
-use rpds::Vector;
+use crate::macros::ident::{ModuleId, Name};
+use rpds::{HashTrieMap, Vector};
 
 /// Represents the ordered table of macros.
 #[derive(Debug, Clone)]
-pub struct MacroTable<T> {
+pub struct MacroTable<M> {
     // XXX currently this is just a persistent Vector, but we could have a variant to Vec
     //     when we "freeze" the module to have O(1) lookup at O(N) one time cost
     //     if we don't do this, access is always O(log N) because we're some kind of trie or
     //     the like under the hood.
-    table: Vector<T>,
+    table: Vector<M>,
 }
 
-impl<T> MacroTable<T> {
+impl<M> MacroTable<M> {
     /// Constructs the empty macro table.
     pub fn empty() -> Self {
         Self {
@@ -38,15 +39,44 @@ impl<T> MacroTable<T> {
         }
     }
 
+    /// Returns the count of elements in the table.
+    pub fn len(&self) -> usize {
+        return self.table.len();
+    }
+
     /// Creates a table the next macro added to it.
-    pub fn push_back(&self, next: T) -> Self {
+    pub fn add_macro(&self, next: M) -> Self {
         Self {
             table: self.table.push_back(next),
         }
     }
 
     /// Retrieves the macro definition at some offset in the table if within bounds.
-    pub fn get(&self, index: usize) -> Option<&T> {
+    pub fn get_macro(&self, index: usize) -> Option<&M> {
         self.table.get(index)
+    }
+}
+
+/// Represents a module of macros.
+#[derive(Debug, Clone)]
+pub struct Module<M> {
+    id: ModuleId,
+    table: MacroTable<M>,
+    index: HashTrieMap<Name, M>,
+}
+
+impl<M> Module<M> {
+    /// Constructs an empty module for some [`ModuleId`].
+    pub fn empty(id: ModuleId) -> Self {
+        Self {
+            id,
+            table: MacroTable::empty(),
+            index: HashTrieMap::new(),
+        }
+    }
+
+    /// Returns the underlying module identifier.
+    pub fn id(&self) -> &ModuleId {
+        &self.id
     }
 }
